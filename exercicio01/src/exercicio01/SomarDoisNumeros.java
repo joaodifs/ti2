837c70267f2e140b8fbb226c07feeb540d322dfa
diasf <diasf@NoteJoao>
2022-08-26 03:41:35 -0300

    SomarTresNumeros (via eclipse)

--- exercicio01/src/exercicio01/SomarDoisNumeros.java
+++ exercicio01/src/exercicio01/SomarDoisNumeros.java
@@ -7,14 +7,16 @@ class SomarDoisNumeros {
 	
 	public static void main (String args[]){
 		//Declaracao de variaveis
-		int num1, num2, soma;
+		int num1, num2, soma, num3;
 		//Leituras
 		System.out.println("Digite um número");
 		num1 = sc.nextInt();
 		System.out.println("Digite outro número");
 		num2 = sc.nextInt();
+		System.out.println("Digite um número");
+		num3 = sc.nextInt();
 		//Somar
-		soma =  num1 +  num2;
+		soma =  num1 +  num2 + num3;
 		//Mostrar na tela
 		System.out.println("Soma:" + soma);          
 		}
